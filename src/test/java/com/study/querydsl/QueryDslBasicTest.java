04e820b72c653124600a8758d3360765a96e2e5d
taem1n1 <youman7953@naver.com>
2022-01-19 19:45:04 +0900

    정렬

--- src/test/java/com/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/com/study/querydsl/QueryDslBasicTest.java
@@ -120,4 +120,32 @@ public class QueryDslBasicTest {
                 .selectFrom(member)
                 .fetchCount();
     }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void sort() {
+
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        Assertions.assertThat(member5.getUsername()).isEqualTo("member5");
+        Assertions.assertThat(member6.getUsername()).isEqualTo("member6");
+        Assertions.assertThat(memberNull.getUsername()).isEqualTo(null);
+    }
 }
